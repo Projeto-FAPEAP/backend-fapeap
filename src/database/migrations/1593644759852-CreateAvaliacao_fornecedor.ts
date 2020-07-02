f45af9f77b10c408bdaa1d534f57a42ff3e0a0b1
leozartino <leozartino@gmail.com>
2020-07-01 22:05:25 -0300

    feat(migrations): add migration de avaliacao_fornecedor

---
+++ src/database/migrations/1593644759852-CreateAvaliacao_fornecedor.ts
@@ -0,0 +1,39 @@
+import { MigrationInterface, QueryRunner, Table } from 'typeorm';
+
+export default class CreateAvaliacaoFornecedor1593644759852
+  implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(
+      new Table({
+        name: 'avaliacao_fornecedor',
+        columns: [
+          {
+            name: 'id',
+            type: 'uuid',
+            isPrimary: true,
+            generationStrategy: 'uuid',
+            default: 'uuid_generate_v4()',
+          },
+          {
+            name: 'estrelas',
+            type: 'integer',
+          },
+          {
+            name: 'created_at',
+            type: 'timestamp',
+            default: 'now()',
+          },
+          {
+            name: 'updated_at',
+            type: 'timestamp',
+            default: 'now()',
+          },
+        ],
+      }),
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropTable('avaliacao_fornecedor');
+  }
+}
