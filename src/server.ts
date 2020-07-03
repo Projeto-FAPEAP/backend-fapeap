6781dccfc5f875f4633b51ab0185a672cd04f0b9
leozartino <leozartino@gmail.com>
2020-07-02 22:56:06 -0300

    style: mudanca na linha 14

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,6 @@
 /* eslint-disable no-console */
 import express from 'express';
-import routes from './routes';
+import routes from './controllers/index.routes';
 
 import './database';
 
@@ -11,5 +11,5 @@ app.use(express.json());
 app.use(routes);
 
 app.listen(PORT, () => {
-  console.log('Server rodando! ⚡');
+  console.log(`Server rodando na porta: ${PORT}! ⚡`);
 });
