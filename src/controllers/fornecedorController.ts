569329b0c0971d601080168957cab8e961fc4d17
leozartino <leozartino@gmail.com>
2020-07-09 17:12:33 -0300

    feat(controllers): add criacao fornecedor

--- src/controllers/fornecedorController.ts
+++ src/controllers/fornecedorController.ts
@@ -1,5 +1,7 @@
+/* eslint-disable prettier/prettier */
 import { getRepository } from 'typeorm';
 import { Request, Response, NextFunction } from 'express';
+import { hash } from 'bcryptjs';
 import Fornecedor from '../models/Fornecedor';
 
 export const listarTodosFornecedores = async (
@@ -9,7 +11,69 @@ export const listarTodosFornecedores = async (
 ): Promise<void> => {
   const fornecedorRepository = getRepository(Fornecedor);
   const fornecedores = await fornecedorRepository.find();
-
   response.status(200).json(fornecedores);
+  next();
+};
+
+export const cadastrarFornecedor = async (
+  request: Request,
+  response: Response,
+  next: NextFunction,
+): Promise<void> => {
+  try {
+    const fornecedorRepository = getRepository(Fornecedor);
+    const {
+      nome_fornecedor,
+      nome_fantasia,
+      cpf_cnpj,
+      email,
+      senha,
+      telefone,
+      telefone_whatsapp,
+      taxa_delivery,
+      logradouro,
+      numero_fornecedor,
+      bairro,
+      cep,
+    } = request.body;
+
+    const checkEmailExists = await fornecedorRepository.findOne({
+      where: { email },
+    });
+
+    const checkCpfExists = await fornecedorRepository.findOne({
+      where: { cpf_cnpj },
+    });
+
+    if (checkEmailExists) {
+      throw new Error('Email já existe!');
+    }
+
+    if (checkCpfExists) {
+      throw new Error('CPF/CNPJ já existe!');
+    }
+
+    const hashedSenha = await hash(senha, 8);
+
+    const fornecedorDTO = fornecedorRepository.create({
+      nome_fornecedor,
+      nome_fantasia,
+      cpf_cnpj,
+      email,
+      senha: hashedSenha,
+      telefone,
+      telefone_whatsapp,
+      taxa_delivery,
+      logradouro,
+      numero_fornecedor,
+      bairro,
+      cep,
+    });
+    const fornecedor = await fornecedorRepository.save(fornecedorDTO);
+    delete fornecedor.senha;
+    response.status(201).json(fornecedor);
+  } catch (error) {
+    response.status(400).json({ error: error.message });
+  }
   next();
 };
