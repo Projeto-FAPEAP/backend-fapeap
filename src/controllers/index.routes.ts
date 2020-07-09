768fdf7c9e77d1b530fd045397cca6001b383b96
leozartino <leozartino@gmail.com>
2020-07-09 17:16:26 -0300

    feat(routes): rotas de fornecedor e consumidor

--- src/controllers/index.routes.ts
+++ src/controllers/index.routes.ts
@@ -1,15 +1,24 @@
 import { Router } from 'express';
-import { listarTodosFornecedores } from './fornecedorController';
+import {
+  listarTodosFornecedores,
+  cadastrarFornecedor,
+} from './fornecedorController';
 import {
   listarTodosConsumidores,
   cadastrarConsumidor,
 } from './consumidorController';
 
+import { autenticaConsumidor, autenticaFornecedor } from './sessaoController';
+
 const routes = Router();
-
+// Fornecedor
+routes.post('/fornecedor', cadastrarFornecedor);
 routes.get('/fornecedor', listarTodosFornecedores);
-
+// Consumidor
 routes.post('/consumidor', cadastrarConsumidor);
 routes.get('/consumidor', listarTodosConsumidores);
+// Sessao
+routes.post('/sessao/consumidor', autenticaConsumidor);
+routes.post('/sessao/fornecedor', autenticaFornecedor);
 
 export default routes;
