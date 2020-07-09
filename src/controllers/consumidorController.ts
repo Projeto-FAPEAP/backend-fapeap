b32e5865cef1bff7fcc5dd327c70a57d6edf1dae
leozartino <leozartino@gmail.com>
2020-07-09 17:12:18 -0300

    feat(controllers): add criacao consumidor

--- src/controllers/consumidorController.ts
+++ src/controllers/consumidorController.ts
@@ -1,5 +1,6 @@
 import { getRepository } from 'typeorm';
 import { Request, Response, NextFunction } from 'express';
+import { hash } from 'bcryptjs';
 import Consumidor from '../models/Consumidor';
 
 export const listarTodosConsumidores = async (
@@ -9,7 +10,6 @@ export const listarTodosConsumidores = async (
 ): Promise<void> => {
   const consumidorRepository = getRepository(Consumidor);
   const consumidores = await consumidorRepository.find();
-
   response.status(200).json(consumidores);
   next();
 };
@@ -21,11 +21,52 @@ export const cadastrarConsumidor = async (
 ): Promise<void> => {
   try {
     const consumidorRepository = getRepository(Consumidor);
-    const consumidorDTO = consumidorRepository.create(request.body);
+    const {
+      nome,
+      cpf,
+      email,
+      senha,
+      telefone_whatsapp,
+      logradouro,
+      numero_casa,
+      bairro,
+      cep,
+    } = request.body;
+
+    const checkEmailExists = await consumidorRepository.findOne({
+      where: { email },
+    });
+
+    const checkCpfExists = await consumidorRepository.findOne({
+      where: { cpf },
+    });
+
+    if (checkEmailExists) {
+      throw new Error('Email já existe!');
+    }
+
+    if (checkCpfExists) {
+      throw new Error('CPF já existe!');
+    }
+
+    const hashedSenha = await hash(senha, 8);
+
+    const consumidorDTO = consumidorRepository.create({
+      nome,
+      cpf,
+      email,
+      senha: hashedSenha,
+      telefone_whatsapp,
+      logradouro,
+      numero_casa,
+      bairro,
+      cep,
+    });
     const consumidor = await consumidorRepository.save(consumidorDTO);
+    delete consumidor.senha;
     response.status(201).json(consumidor);
   } catch (error) {
-    console.log(error.message);
+    response.status(400).json({ error: error.message });
   }
   next();
 };
