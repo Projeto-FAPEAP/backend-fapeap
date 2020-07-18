a71504876a905a0a2c15387a3b742c44c46a216c
leozartino <leozartino@gmail.com>
2020-07-18 10:11:12 -0300

    feat: add listagem e remocao de consumidor

--- src/controllers/consumidorController.ts
+++ src/controllers/consumidorController.ts
@@ -1,3 +1,4 @@
+/* eslint-disable no-param-reassign */
 import { getRepository } from 'typeorm';
 import { Request, Response, NextFunction } from 'express';
 import { hash } from 'bcryptjs';
@@ -54,6 +55,68 @@ export const cadastrarConsumidor = async (
     const consumidor = await consumidorRepository.save(consumidorDTO);
     delete consumidor.senha;
     response.status(201).json(consumidor);
+  } catch (error) {
+    response.status(400).json({ error: error.message });
+  }
+  next();
+};
+
+export const listarTodosConsumidores = async (
+  _: Request,
+  response: Response,
+  next: NextFunction,
+): Promise<void> => {
+  try {
+    const consumidorRepository = getRepository(Consumidor);
+    const collectionsConsumidores = await consumidorRepository.find();
+    collectionsConsumidores.forEach(consumidor => delete consumidor.senha);
+    response.status(200).json(collectionsConsumidores);
+  } catch (error) {
+    response.status(400).json({ error: error.message });
+  }
+  next();
+};
+
+export const listarConsumidor = async (
+  request: Request,
+  response: Response,
+  next: NextFunction,
+): Promise<void> => {
+  try {
+    const { id } = request.params;
+    const consumidorRepository = getRepository(Consumidor);
+
+    const consumidor = await consumidorRepository.find({
+      where: { id },
+    });
+
+    if (!consumidor) {
+      throw new Error('Consumidor não encontrado!');
+    }
+
+    response.status(200).json(consumidor);
+  } catch (error) {
+    response.status(400).json({ error: error.message });
+  }
+  next();
+};
+
+export const deletarConsumidor = async (
+  request: Request,
+  response: Response,
+  next: NextFunction,
+): Promise<void> => {
+  try {
+    const { id } = request.user;
+
+    if (!id) {
+      throw new Error('Usuário não autenticado!');
+    }
+    const consumidorRepository = getRepository(Consumidor);
+
+    await consumidorRepository.delete(id);
+
+    response.status(200).json({ deleted: 'sucess' });
   } catch (error) {
     response.status(400).json({ error: error.message });
   }
