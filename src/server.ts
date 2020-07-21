0dc45ffd822b7610c808cbc16c042d5110fbf612
leozartino <leozartino@gmail.com>
2020-07-21 20:39:46 -0300

    feat: add campos de limit file no express

--- src/server.ts
+++ src/server.ts
@@ -9,8 +9,8 @@ const app = express();
 const PORT = 3333;
 
 app.use(cors());
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.json({ limit: '50mb' }));
+app.use(express.urlencoded({ extended: true, limit: '50mb' }));
 app.use(routes);
 
 dotenv.config();
