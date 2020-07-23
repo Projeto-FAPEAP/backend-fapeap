9aee9ea07794fa212c1137bb8c539e8f862f9207
leozartino <leozartino@gmail.com>
2020-07-23 18:55:50 -0300

    docs: removendo espacao em branco

--- src/controllers/fornecedorController.ts
+++ src/controllers/fornecedorController.ts
@@ -78,7 +78,6 @@ export const cadastrarFornecedor = async (
     const fornecedor = await fornecedorRepository.save(fornecedorDTO);
 
     const arquivoRepository = getRepository(ArquivoFornecedor);
-    
     request.files.forEach(async elementoFile => {
       const { filename: id, originalname: nome_original, size } = elementoFile;
 
