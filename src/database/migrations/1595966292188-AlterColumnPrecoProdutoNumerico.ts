a1c2765c6bf0dd781d7598acc7429b69b44a7ad1
leozartino <leozartino@gmail.com>
2020-07-28 17:00:33 -0300

    feat: add tipo numerico para preco table produto

--- src/database/migrations/1595966292188-AlterColumnPrecoProdutoNumerico.ts
+++ src/database/migrations/1595966292188-AlterColumnPrecoProdutoNumerico.ts
@@ -1,6 +1,6 @@
 import { MigrationInterface, QueryRunner, TableColumn } from 'typeorm';
 
-export default class AlterColumnPrecoProduto1595442112394
+export default class AlterColumnPrecoProdutoNumerico1595966292188
   implements MigrationInterface {
   public async up(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.dropColumn('produto', 'preco');
