00495fa656ced8f94728bc81ddb36f368998b537
leozartino <leozartino@gmail.com>
2020-08-01 12:55:52 -0300

    feat: add configs do build

--- ormconfig.js
+++ ormconfig.js
@@ -1,8 +1,8 @@
 module.exports = {
   type: 'postgres',
   url: process.env.DATABASE_URL,
-  entities: ['src/models/**/*.ts'],
-  migrations: ['src/database/migrations/**/*.ts'],
+  entities: ['dist/models/**/*.js'],
+  migrations: ['dist/database/migrations/**/*.js'],
   cli: {
     migrationsDir: './src/database/migrations',
   },
