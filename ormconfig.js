5635aa381a1725565a3caaa886cd670438176c5b
leozartino <leozartino@gmail.com>
2020-08-03 09:23:06 -0300

    feat: configs de ambiente desenvolvimento

--- ormconfig.js
+++ ormconfig.js
@@ -1,8 +1,8 @@
 module.exports = {
   type: 'postgres',
   url: process.env.DATABASE_URL,
-  entities: ['dist/models/**/*.js'],
-  migrations: ['dist/database/migrations/**/*.js'],
+  entities: ['src/models/**/*.ts'],
+  migrations: ['src/database/migrations/**/*.ts'],
   cli: {
     migrationsDir: './src/database/migrations',
   },
