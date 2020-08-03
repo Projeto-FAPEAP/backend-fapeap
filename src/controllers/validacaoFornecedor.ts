3b01e1673a6ada80a68eb1c26d5c1a46c7f0f50c
leozartino <leozartino@gmail.com>
2020-08-03 16:17:57 -0300

    feat: add validacao fornecedor dashboard

--- src/controllers/validacaoFornecedor.ts
+++ src/controllers/validacaoFornecedor.ts
@@ -1,8 +1,10 @@
 /* eslint-disable no-param-reassign */
+/* eslint-disable no-await-in-loop */
 
 import { getRepository } from 'typeorm';
 import { Request, Response, NextFunction } from 'express';
 import Fornecedor from '../models/Fornecedor';
+import ArquivoFornecedor from '../models/ArquivoFornecedor';
 
 export const listarForneceodresNaoVerificados = async (
   _: Request,
@@ -12,12 +14,27 @@ export const listarForneceodresNaoVerificados = async (
   try {
     // Carregar url dos arquivos dos fornecedores
     const fornecedorRepository = getRepository(Fornecedor);
-    const collectionsFornecedores = await fornecedorRepository.find({
+    const arquivoFornecedorRepository = getRepository(ArquivoFornecedor);
+    const fornecedores = await fornecedorRepository.find({
       where: { verificado: false },
     });
-    collectionsFornecedores.forEach(fornecedor => delete fornecedor.senha);
 
-    response.status(200).json(collectionsFornecedores);
+    const resultado = [];
+
+    // eslint-disable-next-line no-restricted-syntax
+    for (const fornece of fornecedores) {
+      const arquivos = await arquivoFornecedorRepository.find({
+        where: { fornecedor_id: fornece.id },
+      });
+
+      delete fornece.senha;
+      arquivos.forEach(arq => delete arq.fornecedor);
+
+      resultado.push(fornece);
+      resultado.push({ arquivos });
+    }
+
+    response.status(200).json(resultado);
   } catch (error) {
     response.status(400).json({ error: error.message });
   }
